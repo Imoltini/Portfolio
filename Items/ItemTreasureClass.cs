0020ac185273a7b3c85dcf8e0ada0ff01ec9738b
Imoltini <42342392+Imoltini@users.noreply.github.com>
2023-07-05 23:31:35 +0800

    Add files via upload

---
+++ Items/ItemTreasureClass.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item Treasure Class", menuName = "Items/Treasure Classes/Item Treasure Class")]
+public class ItemTreasureClass : ScriptableObject
+{
+    [Header("Treasure Class Variables")]
+    public int tcLevel;
+    public string tcName;
+    //
+    [Header("Items and Drop Chances")]
+    public ItemObject[] itemObjects;
+    public int[] itemPickChance;
+    //
+    [Header("Probability Sums")]
+    public int sumItemPickProb;
+
+    //
+
+    void OnValidate()
+    {
+        tcName = name;
+        if (itemPickChance.Length > 0) CalculateSumItemPickProbability();
+    }
+    //
+    void CalculateSumItemPickProbability()
+    {
+        sumItemPickProb = 0;
+        for (int i = 0; i < itemPickChance.Length; i++)
+        {
+            sumItemPickProb += itemPickChance[i];
+        }
+    }
+}
