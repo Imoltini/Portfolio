c36675e68006067ff82ae6e6fb63543cbfe7ca7a
Imoltini <42342392+Imoltini@users.noreply.github.com>
2023-07-05 23:29:13 +0800

    Add files via upload

---
+++ Items/ItemType.cs
@@ -0,0 +1,64 @@
+public enum ItemType
+{
+    Equipment,
+    Weapon,
+    Artifact,
+    Enchanting,
+    Cosmetics,
+    DominationRune,
+    Offhand
+}
+//
+public enum ArmorType
+{
+    Pants = 0,
+    Boots = 1,
+    Gloves = 2,
+    Helmet = 3,
+    Ring = 4,
+    Shoulders = 5,
+    Chest = 6,
+    Shield = 7,
+    HelmetAttachments = 8,
+    Elbows = 9,
+    Knees = 10,
+    Backs = 11,
+    Hips = 12,
+    None = 99
+}
+//
+public enum CosmeticsType
+{
+    Head,
+    Hips,
+    Wings,
+    Outfit,
+    Sanctuary,
+    Companion
+}
+//
+public enum HelmetType
+{
+    None,
+    NoHair,
+    NoNothing,
+    NoFacialHair
+}
+//
+public enum ArtifactType
+{
+    Ancient,
+    Enchanted,
+    Desolate,
+    Lunar,
+    Cursed
+}
+//
+public enum GlyphType
+{
+    Temporal,
+    Ethereal,
+    Astral,
+    Celestial,
+    Cosmic
+}
